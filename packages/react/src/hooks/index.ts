b36d2ad6ce7f63973283a82212c9820c18ce2ddf
bowheart <jclaunch@trumidtech.com>
2021-05-03 10:17:35 -0600

    add ecosystem hook

--- packages/react/src/hooks/index.ts
+++ packages/react/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useAtomContext'
 export * from './useAtomWithoutSubscription'
 export * from './useAtomWithSubscription'
+export * from './useEcosystem'
---
+++ packages/react/src/hooks/useEcosystem.ts
@@ -0,0 +1,4 @@
+import { useContext } from 'react'
+import { ecosystemContext } from '../classes'
+
+export const useEcosystem = () => useContext(ecosystemContext)
