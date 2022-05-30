9f5c62b61b163b7e37c059c7d8c96bc7b498e1d3
bowheart <bowheart31@gmail.com>
2022-05-30 11:18:49 -0600

    make createEcosystem return an existing ecosystem with the given id

--- packages/react/src/factories/createEcosystem.tsx
+++ packages/react/src/factories/createEcosystem.tsx
@@ -1,8 +1,24 @@
 import { Ecosystem } from '../classes/Ecosystem'
+import { addEcosystem, globalStore } from '../store'
+import { getEcosystem } from '../store/public-api'
 import { EcosystemConfig } from '../types'
 
 export const createEcosystem = <
   Context extends Record<string, any> | undefined = any
 >(
   config: EcosystemConfig<Context> = {}
-) => new Ecosystem<Context>(config)
+) => {
+  if (config?.id) {
+    const ecosystem = getEcosystem(config.id)
+
+    if (ecosystem) return ecosystem
+  }
+
+  const ecosystem = new Ecosystem<Context>(config)
+
+  // yep. Dispatch this here. We'll make sure no component can ever be updated
+  // synchronously from this call (causing update-during-render react warnings)
+  globalStore.dispatch(addEcosystem(ecosystem))
+
+  return ecosystem
+}
