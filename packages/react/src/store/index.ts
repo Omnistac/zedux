484e1f9dd1cabe7b7d685296355b2f440bc46577
bowheart <jclaunch@trumidtech.com>
2022-05-30 21:44:05 -0600

    automatically register child windows

--- packages/react/src/store/index.ts
+++ packages/react/src/store/index.ts
@@ -1,8 +1,38 @@
-import { createStore } from '@zedux/core'
+import { createStore, Store } from '@zedux/core'
 import { ecosystemsReducer } from './ecosystems'
 
 export * from './actions'
 
-export const globalStore = createStore({
-  ecosystems: ecosystemsReducer,
-})
+const eventType = '@@zedux/register-child-window'
+
+type GlobalStore = Store<{ ecosystems: ReturnType<typeof ecosystemsReducer> }>
+
+const getGlobalStore = (): GlobalStore => {
+  if (typeof window === 'undefined') {
+    return createStore({ ecosystems: ecosystemsReducer })
+  }
+
+  if (typeof window.addEventListener !== 'undefined') {
+    window.addEventListener(eventType, event => {
+      if (typeof (event as CustomEvent).detail?.callback === 'function') {
+        ;(event as CustomEvent).detail.callback(globalStore)
+      }
+    })
+  }
+
+  if (window.opener && window.dispatchEvent && window.CustomEvent) {
+    let store: Store | undefined = undefined
+    const callback = (storeFromAbove: Store) => (store = storeFromAbove)
+
+    const event = new window.CustomEvent(eventType, { detail: { callback } })
+    window.opener.dispatchEvent(event)
+
+    if (store) return store
+  }
+
+  return createStore({
+    ecosystems: ecosystemsReducer,
+  })
+}
+
+export const globalStore = getGlobalStore()
