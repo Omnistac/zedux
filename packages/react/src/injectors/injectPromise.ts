e3e02329bda9d1991c4a8e6fed6417708cafd441
bowheart <bowheart31@gmail.com>
2022-09-08 09:58:14 -0400

    improve injectPromise - add initialState and dataOnly options and fix bugs

--- packages/react/src/injectors/injectPromise.ts
+++ packages/react/src/injectors/injectPromise.ts
@@ -1,35 +1,58 @@
+import { Store } from '@zedux/core'
+import { detailedTypeof } from '@zedux/core/utils/general'
 import { api } from '../factories'
 import {
   getErrorPromiseState,
   getInitialPromiseState,
   getSuccessPromiseState,
 } from '../utils/promiseUtils'
-import { InjectorDeps, InjectStoreConfig } from '../types'
+import { InjectorDeps, InjectStoreConfig, PromiseState } from '../types'
 import { injectEffect } from './injectEffect'
 import { injectMemo } from './injectMemo'
 import { injectStore } from './injectStore'
 import { injectRef } from './injectRef'
-import { detailedTypeof } from '@zedux/core/utils/general'
+import { StoreAtomApi } from '../classes'
 
-export const injectPromise = <T>(
+/**
+ * Create a memoized promise reference
+ */
+export const injectPromise: {
+  <T>(
+    getPromise: (controller?: AbortController) => Promise<T>,
+    deps: InjectorDeps,
+    config: { initialState?: T; dataOnly: true } & InjectStoreConfig
+  ): StoreAtomApi<Store<T>, Record<string, any>, Promise<T>>
+
+  <T>(
+    getPromise: (controller?: AbortController) => Promise<T>,
+    deps?: InjectorDeps,
+    config?: { initialState?: T; dataOnly?: boolean } & InjectStoreConfig
+  ): StoreAtomApi<Store<PromiseState<T>>, Record<string, any>, Promise<T>>
+} = <T>(
   getPromise: (controller?: AbortController) => Promise<T>,
   deps?: InjectorDeps,
-  storeConfig?: InjectStoreConfig
+  {
+    initialState,
+    dataOnly,
+    ...storeConfig
+  }: { initialState?: T; dataOnly?: boolean } & InjectStoreConfig = {}
 ) => {
-  const controller = injectMemo(
-    () =>
-      typeof AbortController !== 'undefined'
-        ? new AbortController()
-        : undefined,
-    []
-  )
+  const controllerRef = injectRef<AbortController>()
   const promiseRef = injectRef<Promise<T>>()
 
-  const store = injectStore(getInitialPromiseState<T>(), storeConfig)
+  const store = injectStore(
+    dataOnly ? initialState : getInitialPromiseState<T>(initialState),
+    storeConfig
+  )
 
   // setting a ref during evaluation is perfectly fine in Zedux
   promiseRef.current = injectMemo(() => {
-    const promise = getPromise(controller)
+    const prevController = controllerRef.current
+    const nextController =
+      typeof AbortController !== 'undefined' ? new AbortController() : undefined
+
+    controllerRef.current = nextController
+    const promise = getPromise(controllerRef.current)
 
     if (DEV && typeof promise?.then !== 'function') {
       throw new TypeError(
@@ -41,25 +64,31 @@ export const injectPromise = <T>(
 
     if (promise === promiseRef.current) return promiseRef.current
 
-    store.setState(getInitialPromiseState())
+    prevController?.abort('updated')
+
+    if (!dataOnly) {
+      // preserve previous data and error using setStateDeep:
+      store.setStateDeep(getInitialPromiseState())
+    }
 
     promise
       .then(data => {
-        if (promiseRef.current !== promise) return
+        if (nextController?.signal.aborted) return
 
-        store.setState(getSuccessPromiseState(data))
+        store.setState(dataOnly ? data : getSuccessPromiseState(data))
       })
       .catch(error => {
-        if (promiseRef.current !== promise) return
+        if (dataOnly || nextController?.signal.aborted) return
 
-        store.setState(getErrorPromiseState(error))
+        // preserve previous data using setStateDeep:
+        store.setStateDeep(getErrorPromiseState(error))
       })
 
     return promise
   }, deps)
 
   injectEffect(
-    () => () => (controller?.abort as any)?.('atom instance destroyed'),
+    () => () => (controllerRef.current?.abort as any)?.('destroyed'),
     []
   )
 
