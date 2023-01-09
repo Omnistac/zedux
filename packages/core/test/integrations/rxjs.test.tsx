fafcd74e71d1447ca433caa42a5e273bc8a331b7
Joshua Claunch <bowheart31@gmail.com>
2023-01-08 21:53:32 -0500

    add some rxjs integration tests

--- packages/core/test/integrations/rxjs.test.tsx
+++ packages/core/test/integrations/rxjs.test.tsx
@@ -18,12 +18,66 @@ describe('rxjs integration', () => {
     expect((store as any)._subscribers.length).toBe(0)
   })
 
+  test('store -> observable of state (via Symbol.observable)', () => {
+    const store = createStore(null, 'a')
+    const state$ = from(store[Symbol.observable]())
+
+    const emissions: string[] = []
+    const subscription = state$.subscribe({ next: val => emissions.push(val) })
+
+    store.setState('b')
+    store.setState('c')
+    subscription.unsubscribe()
+    store.setState('d')
+
+    expect(emissions).toEqual(['b', 'c'])
+    expect((store as any)._subscribers.length).toBe(0)
+  })
+
   test('store -> observable of actions', () => {
     const store = createStore(null, 'a')
     const action$ = from(store.actionStream())
 
     const emissions: ActionChain[] = []
     const subscription = action$.subscribe({ next: val => emissions.push(val) })
+
+    store.setState('b')
+    store.dispatch({ type: 'c', payload: 1 })
+    subscription.unsubscribe()
+    store.setState('d')
+
+    expect(emissions).toEqual([
+      { type: actionTypes.HYDRATE, payload: 'b' },
+      { type: 'c', payload: 1 },
+    ])
+    expect((store as any)._subscribers.length).toBe(0)
+  })
+
+  test('store -> observable of actions (via Symbol.observable)', () => {
+    const store = createStore(null, 'a')
+    const action$ = from(store.actionStream()[Symbol.observable]())
+
+    const emissions: ActionChain[] = []
+    const subscription = action$.subscribe({ next: val => emissions.push(val) })
+
+    store.setState('b')
+    store.dispatch({ type: 'c', payload: 1 })
+    subscription.unsubscribe()
+    store.setState('d')
+
+    expect(emissions).toEqual([
+      { type: actionTypes.HYDRATE, payload: 'b' },
+      { type: 'c', payload: 1 },
+    ])
+    expect((store as any)._subscribers.length).toBe(0)
+  })
+
+  test('store -> observable of actions with custom subscriber', () => {
+    const store = createStore(null, 'a')
+    const action$ = store.actionStream()[Symbol.observable]()
+
+    const emissions: ActionChain[] = []
+    const subscription = action$.subscribe(val => emissions.push(val))
 
     store.setState('b')
     store.dispatch({ type: 'c', payload: 1 })
@@ -56,5 +110,57 @@ describe('rxjs integration', () => {
       [{ type: 'c', payload: 1 }, 'b'],
     ])
     expect((store as any)._subscribers.length).toBe(0)
+  })
+
+  test('reducer errors emit error signals on action streams', () => {
+    const reducer = (state: any) => {
+      if (state) throw 'a'
+
+      return 'b'
+    }
+
+    const next = jest.fn()
+    const error = jest.fn()
+
+    const store = createStore(reducer)
+    const action$ = from(store.actionStream())
+    const subscription = action$.subscribe({
+      next,
+      error,
+    })
+
+    try {
+      store.dispatch({ type: 'test' })
+    } catch (err) {}
+
+    subscription.unsubscribe()
+
+    expect(next).not.toHaveBeenCalled()
+    expect(error).toHaveBeenCalledTimes(1)
+    expect(error).toHaveBeenCalledWith('a')
+  })
+
+  test('reducer errors emit nothing if no error subscriber is passed', () => {
+    const reducer = (state: any) => {
+      if (state) throw 'a'
+
+      return 'b'
+    }
+
+    const next = jest.fn()
+
+    const store = createStore(reducer)
+    const action$ = store.actionStream()['@@observable']()
+    const subscription = action$.subscribe({
+      next,
+    })
+
+    try {
+      store.dispatch({ type: 'test' })
+    } catch (err) {}
+
+    subscription.unsubscribe()
+
+    expect(next).not.toHaveBeenCalled()
   })
 })
