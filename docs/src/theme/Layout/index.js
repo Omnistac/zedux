2c1201a3ad331bfb851f697b9d9354c9aefac854
Joshua Claunch <bowheart31@gmail.com>
2023-01-27 14:36:43 -0500

    more ssg style fixes

--- docs/src/theme/Layout/index.js
+++ docs/src/theme/Layout/index.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { useLocation } from '@docusaurus/router'
 import Layout from '@theme-original/Layout'
 import { ThemeProvider } from '@site/src/ssc'
 
+const head =
+  typeof document === 'undefined'
+    ? { querySelector: () => null }
+    : document.head
+
 export default function LayoutWrapper(props) {
+  const [isHidden, setIsHidden] = useState(head.querySelector('[data-ssc]'))
+
+  useLayoutEffect(() => {
+    if (head.querySelector('[data-ssc]')) {
+      return setIsHidden(false)
+    }
+
+    const observer = new MutationObserver(() => {
+      if (head.querySelector('[data-ssc]')) {
+        setIsHidden(false)
+      }
+    })
+
+    observer.observe(head, { childList: true })
+
+    return () => observer.disconnect()
+  }, [])
+
   return (
     <>
       <ThemeProvider id={useLocation().pathname.replace(/[^a-zA-Z]/g, '_')}>
-        <Layout {...props} />
+        <div
+          className={isHidden ? 'ssc-hide' : ''}
+          style={{ display: 'contents' }}
+        >
+          <Layout {...props} />
+        </div>
       </ThemeProvider>
     </>
   )
