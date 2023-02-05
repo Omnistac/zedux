1724d9e406c038e20c717862203fbf44842f0faf
Joshua Claunch <bowheart31@gmail.com>
2023-02-04 22:19:48 -0500

    add TOC items for API doc legend rows

---
+++ docs/src/theme/DocItem/TOC/Desktop/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ThemeClassNames } from '@docusaurus/theme-common'
+import { useDoc } from '@docusaurus/theme-common/internal'
+import TOC from '@theme/TOC'
+export default function DocItemTOCDesktop() {
+  const { toc, frontMatter } = useDoc()
+  const clonedToc = [...toc]
+
+  const otherElements = document.querySelectorAll('.markdown > * > div.anchor')
+
+  ;[...otherElements].reverse().forEach(el => {
+    let prevAnchor = el.parentElement.previousElementSibling
+
+    while (prevAnchor && !prevAnchor.classList.contains('anchor')) {
+      prevAnchor = prevAnchor.previousElementSibling
+    }
+
+    if (!prevAnchor) return
+
+    const prevAnchorIndex = clonedToc.findIndex(
+      item => item.id === prevAnchor.id
+    )
+
+    if (prevAnchorIndex === -1) return
+
+    clonedToc.splice(prevAnchorIndex + 1, 0, {
+      id: el.id,
+      level: 3,
+      value: el.innerHTML,
+    })
+  })
+
+  console.log('the toc:', toc)
+  return (
+    <TOC
+      toc={clonedToc}
+      minHeadingLevel={frontMatter.toc_min_heading_level}
+      maxHeadingLevel={frontMatter.toc_max_heading_level}
+      className={ThemeClassNames.docs.docTocDesktop}
+    />
+  )
+}
