f5540694d73e30bf87cbfaa2853d8cc744a74f0a
Joshua Claunch <bowheart31@gmail.com>
2023-03-24 18:12:29 -0400

    add suspense error tests

--- packages/react/test/integrations/suspense.test.tsx
+++ packages/react/test/integrations/suspense.test.tsx
@@ -1,5 +1,6 @@
 import { api, atom, createEcosystem, useAtomValue } from '@zedux/react'
 import React, { Suspense } from 'react'
+import { ErrorBoundary } from '../utils/ErrorBoundary'
 import { renderInEcosystem } from '../utils/renderInEcosystem'
 
 const ecosystem = createEcosystem({ id: 'test' })
@@ -67,5 +68,87 @@ describe('suspense', () => {
     const div2 = await findByTestId('b')
 
     expect(div2.innerHTML).toBe('b')
+  })
+
+  test('api().setPromise() rejected error hits ErrorBoundary', async () => {
+    const originalConsoleError = console.error
+    const mock = (console.error = jest.fn())
+
+    let reject: (val?: any) => void = () => {}
+
+    const atom1 = atom('1', () => {
+      return api('b').setPromise(new Promise((_, r) => (reject = r)))
+    })
+
+    const Child = () => <div data-testid="b">{useAtomValue(atom1)}</div>
+
+    const Parent = () => (
+      <ErrorBoundary>
+        <Suspense fallback={<div data-testid="a">a</div>}>
+          <Child />
+        </Suspense>
+      </ErrorBoundary>
+    )
+
+    const { findByTestId } = renderInEcosystem(<Parent />, ecosystem)
+
+    const div1 = await findByTestId('a')
+
+    expect(div1.innerHTML).toBe('a')
+
+    reject('b')
+
+    const div2 = await findByTestId('error')
+
+    expect(div2.innerHTML).toBe('b')
+    expect(mock).toHaveBeenCalledTimes(3)
+    expect(mock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(/uncaught 'b'/i),
+      'b'
+    )
+
+    console.error = originalConsoleError
+  })
+
+  test('api(promise) rejected error hits ErrorBoundary', async () => {
+    const originalConsoleError = console.error
+    const mock = (console.error = jest.fn())
+
+    let reject: (val?: any) => void = () => {}
+
+    const atom1 = atom('1', () => {
+      return api(new Promise<string>((_, r) => (reject = r)))
+    })
+
+    const Child = () => <div data-testid="b">{useAtomValue(atom1).data}</div>
+
+    const Parent = () => (
+      <ErrorBoundary>
+        <Suspense fallback={<div data-testid="a">a</div>}>
+          <Child />
+        </Suspense>
+      </ErrorBoundary>
+    )
+
+    const { findByTestId } = renderInEcosystem(<Parent />, ecosystem)
+
+    const div1 = await findByTestId('a')
+
+    expect(div1.innerHTML).toBe('a')
+
+    reject('b')
+
+    const div2 = await findByTestId('error')
+
+    expect(div2.innerHTML).toBe('b')
+    expect(mock).toHaveBeenCalledTimes(3)
+    expect(mock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringMatching(/uncaught 'b'/i),
+      'b'
+    )
+
+    console.error = originalConsoleError
   })
 })
