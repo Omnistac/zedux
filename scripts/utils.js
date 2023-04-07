bc61c61ba833110e6f3185a192f25eca8b2f040f
Joshua Claunch <bowheart31@gmail.com>
2023-04-07 19:57:32 -0400

    chore: implement release script and update commit script accordingly

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,11 @@
 import { exec } from 'child_process'
 import { promises } from 'fs'
-import https from 'https'
+import { request } from 'https'
+import inquirer from 'inquirer'
+import path from 'path'
 import { promisify } from 'util'
 
-export const { readdir, readFile } = promises
+export const { readdir, readFile, writeFile } = promises
 
 /**
  * Make sure this script is invoked from the repo root
@@ -21,19 +23,87 @@ export const assertCwdIsRoot = async () => {
 
 const execAsync = promisify(exec)
 
-export const cmd = command => execAsync(command).catch(err => ({ stderr: err }))
+/**
+ * Run any command in the CWD.
+ */
+export const cmd = command =>
+  execAsync(command).catch(err => ({ stderr: err, stdout: '' }))
 
-export const get = (url, options) =>
+/**
+ * Ask the user a question and wait for a yes/no response. Pass the 2nd param to
+ * set the default if they hit enter without typing anything.
+ */
+export const confirm = async (message, theDefault = true) => {
+  const { proceed } = await inquirer.prompt([
+    {
+      default: theDefault,
+      message,
+      name: 'proceed',
+      type: 'confirm',
+    },
+  ])
+
+  if (!proceed) await die()
+}
+
+/**
+ * Kill the process, undoing changes. Can only be invoked before we `git push`
+ */
+const die = async () => {
+  console.info('Canceling')
+  await cmd('git checkout -- .')
+  process.exit(0)
+}
+
+/**
+ * Accepts a url string, a typical `fetch`/`request` options object, and
+ * optional `postData` object/array.
+ *
+ * Sends the request over https and parses the response body (if any).
+ *
+ * Returns a `{ body, headers, statusCode }` object
+ */
+export const fetch = (url, options = {}, postData) =>
   new Promise((resolve, reject) => {
-    let buffer = ''
+    let body = []
 
-    https
-      .get(url, options, res => {
-        res.on('data', data => {
-          buffer += data.toString()
-        })
-
-        res.on('end', () => resolve(buffer))
+    const req = request(url, options, res => {
+      res.on('data', data => {
+        body.push(data)
       })
-      .on('error', err => reject(err))
+
+      res.on('end', () => {
+        const bodyString = Buffer.concat(body).toString()
+
+        resolve({
+          body: bodyString ? JSON.parse(bodyString) : undefined,
+          headers: res.headers,
+          statusCode: res.statusCode,
+        })
+      })
+    })
+
+    req.on('error', err => reject(err))
+
+    if (postData) req.write(JSON.stringify(postData))
+
+    req.end()
   })
+
+/**
+ * Get the list of package names in the monorepo. These are the folder names
+ * inside the `packages/` directory.
+ */
+export const getPackages = async () => {
+  console.info('Reading available packages')
+
+  return (await readdir(path.resolve('packages'), { withFileTypes: true }))
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name)
+}
+
+export const readJson = async (...args) =>
+  JSON.parse((await readFile(...args)).toString())
+
+export const writeJson = (fileName, data) =>
+  writeFile(fileName, JSON.stringify(data, null, 2) + '\n')
