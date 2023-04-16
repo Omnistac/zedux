8aeb4886a6d9c8bc4f64cd25ce2a0b5008138411
Joshua Claunch <bowheart31@gmail.com>
2023-04-15 21:00:00 -0400

    feat(react): export SelectorCache class

--- packages/react/src/classes/index.ts
+++ packages/react/src/classes/index.ts
@@ -1,8 +1,9 @@
-export * from './templates/IonTemplate' // this .. has to be imported before AtomTemplate or the build breaks :exploding_head:
-export * from './templates/AtomTemplate'
-export * from './templates/AtomTemplateBase'
 export * from './instances/AtomInstance'
 export * from './instances/AtomInstanceBase'
+export * from './templates/IonTemplate' // this has to be imported before AtomTemplate or the build breaks
+export * from './templates/AtomTemplate'
+export * from './templates/AtomTemplateBase'
 export * from './AtomApi'
 export * from './Ecosystem'
 export * from './ZeduxPlugin'
+export { SelectorCache } from './Selectors'
