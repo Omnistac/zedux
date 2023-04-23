8e3cbd84a9983b1d12eae2b01aff943f5826e729
Joshua Claunch <bowheart31@gmail.com>
2023-04-23 14:58:18 -0400

    feat!: break out state machines into their own `@zedux/machines` package
    @affects atoms, core, machines

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,4 @@
 import { Store } from './api/createStore'
-import { MachineStore } from './api/MachineStore'
 
 // Same workaround rxjs uses for Symbol.observable:
 declare global {
@@ -135,40 +134,6 @@ export interface Job {
    */
   type: 0 | 1 | 2 | 3 | 4
 }
-
-export type MachineHook<
-  StateNames extends string,
-  EventNames extends string,
-  Context extends Record<string, any> | undefined
-> = (
-  store: MachineStore<StateNames, EventNames, Context>,
-  storeEffect: StoreEffect<
-    MachineStateShape<StateNames, Context>,
-    MachineStore<StateNames, EventNames, Context>
-  >
-) => void
-
-export interface MachineStateShape<
-  StateNames extends string = string,
-  Context extends Record<string, any> | undefined = undefined
-> {
-  value: StateNames
-  context: Context
-}
-
-export type MachineStoreContextType<M extends MachineStore> =
-  M extends MachineStore<any, any, infer C> ? C : never
-
-export type MachineStoreEventNamesType<M extends MachineStore> =
-  M extends MachineStore<any, infer E, any> ? E : never
-
-export type MachineStoreStateType<M extends MachineStore> =
-  M extends MachineStore<infer S, any, infer C>
-    ? MachineStateShape<S, C>
-    : never
-
-export type MachineStoreStateNamesType<M extends MachineStore> =
-  M extends MachineStore<infer S, any, any> ? S : never
 
 export type NextSubscriber<State = any> = (
   newState: State,
