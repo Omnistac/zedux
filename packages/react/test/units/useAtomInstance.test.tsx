c237502372150a8d148da864ca495519f3e74486
Joshua Claunch <bowheart31@gmail.com>
2023-05-05 20:50:53 -0400

    test(react): add test for React recreating a tree containing a `ttl: 0` atom

--- packages/react/test/units/useAtomInstance.test.tsx
+++ packages/react/test/units/useAtomInstance.test.tsx
@@ -1,11 +1,11 @@
 import { act } from '@testing-library/react'
-import { atom, useAtomInstance } from '@zedux/react'
+import { atom, useAtomInstance, useAtomValue } from '@zedux/react'
 import React from 'react'
 import { ecosystem } from '../utils/ecosystem'
 import { renderInEcosystem } from '../utils/renderInEcosystem'
 
 describe('useAtomInstance', () => {
-  test('passing { subscribe: true } subscribes', async () => {
+  test('passing `subscribe: true` subscribes', async () => {
     jest.useFakeTimers()
     const atom1 = atom('1', 'a')
 
@@ -58,5 +58,47 @@ describe('useAtomInstance', () => {
     })
 
     expect(div.innerHTML).toBe('a')
+  })
+
+  test('recovers from React replacing a component tree, making refCount go from 1 to 0 to 1, when the atom has `ttl: 0`', async () => {
+    jest.useFakeTimers()
+    const atom1 = atom('1', 'a', { ttl: 0 })
+    const toggleAtom = atom('toggle', true)
+
+    function TestA() {
+      const instance = useAtomInstance(atom1, [], { subscribe: true })
+
+      return <div data-testid="text">{instance.getState()}</div>
+    }
+
+    function TestB() {
+      const instance = useAtomInstance(atom1, [], { subscribe: true })
+
+      return <div data-testid="text">{instance.getState()}</div>
+    }
+
+    function Parent() {
+      const showA = useAtomValue(toggleAtom)
+
+      return showA ? <TestA /> : <TestB />
+    }
+
+    const { findByTestId } = renderInEcosystem(<Parent />)
+
+    expect((await findByTestId('text')).innerHTML).toBe('a')
+
+    act(() => {
+      ecosystem.getInstance(toggleAtom).setState(false)
+      jest.runAllTimers()
+    })
+
+    expect((await findByTestId('text')).innerHTML).toBe('a')
+
+    act(() => {
+      ecosystem.getInstance(atom1).setState('b')
+      jest.runAllTimers()
+    })
+
+    expect((await findByTestId('text')).innerHTML).toBe('b')
   })
 })
