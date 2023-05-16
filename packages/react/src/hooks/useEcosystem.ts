5701003194ea1395bce0d06656eac73f69d55836
Joshua Claunch <bowheart31@gmail.com>
2023-05-15 23:28:29 -0400

    docs(react): add `useEcosystem` jsdoc

--- packages/react/src/hooks/useEcosystem.ts
+++ packages/react/src/hooks/useEcosystem.ts
@@ -2,6 +2,18 @@ import { createEcosystem, getEcosystem } from '@zedux/atoms'
 import { useContext } from 'react'
 import { ecosystemContext } from '../utils'
 
+/**
+ * Get the current ecosystem controlling atom usages in this component.
+ *
+ * If an ecosystem has been provided via an `<EcosystemProvider>` above the
+ * current component, `useEcosystem()` returns a reference to the provided
+ * ecosystem.
+ *
+ * If no ecosystem has been provided, `useEcosystem()` returns the global
+ * ecosystem (creating it if it hasn't been created yet).
+ *
+ * Returns an Ecosystem class instance.
+ */
 export const useEcosystem = () => {
   const id = useContext(ecosystemContext)
 
