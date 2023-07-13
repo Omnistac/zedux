058bd5333606e1ed8450d02f2c10e0c308e33e10
Josh C <bowheart31@gmail.com>
2023-07-13 06:59:12 -0400

    feat(atoms): deprecate `injectInvalidate` (prefer `injectSelf`) (#70)

--- packages/atoms/src/injectors/injectInvalidate.ts
+++ packages/atoms/src/injectors/injectInvalidate.ts
@@ -1,5 +1,18 @@
 import { readInstance } from '../classes/EvaluationStack'
 
+/**
+ * Returns a function that can be called to invalidate the current atom
+ * instance.
+ *
+ * @deprecated This injector will be removed in the next major release. Use the
+ * following pattern instead:
+ *
+ * ```ts
+ * const self = injectSelf()
+ * // then in a callback or effect:
+ * self.invalidate()
+ * ```
+ */
 export const injectInvalidate = () => {
   const instance = readInstance()
 
