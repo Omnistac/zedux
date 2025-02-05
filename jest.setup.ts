05e98496d052a4568c5d35881cae4b068983604e
Joshua Claunch <bowheart31@gmail.com>
2025-02-05 11:56:12 -0500

    test(react): add regression tests for recent issues

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,13 +2,32 @@ import '@testing-library/jest-dom/extend-expect'
 
 let callstacks: Record<string, string> = {}
 let id = 0
+let prevIds: string[] = []
+let useIdMode: 'overridden' | 'react19' | 'react18' = 'overridden'
 
 afterEach(() => {
   callstacks = {}
   id = 0
+  prevIds = []
+  useIdMode = 'overridden'
 })
 
+const react = jest.requireActual('react')
+
 const generateId = () => {
+  if (useIdMode === 'react19') return react.useId()
+
+  if (useIdMode === 'react18') {
+    const id1 = react.useId()
+    const id2 = react.useId()
+
+    if (prevIds.includes(id1)) return id2
+
+    prevIds.push(id1)
+
+    return id1
+  }
+
   const stack =
     (new Error().stack || '')
       .split('\n')
@@ -18,7 +37,7 @@ const generateId = () => {
 }
 
 jest.mock('react', () => ({
-  ...jest.requireActual('react'),
+  ...react,
   useId: generateId,
 }))
 
@@ -33,4 +52,10 @@ jest.mock('react', () => ({
   if (key) {
     delete callstacks[key]
   }
+}
+;(globalThis as any).useReact19UseId = () => {
+  useIdMode = 'react19'
+}
+;(globalThis as any).useReact18UseId = () => {
+  useIdMode = 'react18'
 }
---
+++ packages/react/test/regressions/160.test.tsx
@@ -0,0 +1,121 @@
+import {
+  atom,
+  AtomGetters,
+  AtomInstanceType,
+  AtomProvider,
+  useAtomContext,
+  useAtomInstance,
+  useAtomSelector,
+} from '@zedux/react'
+import React from 'react'
+import { renderInEcosystem } from '../utils/renderInEcosystem'
+
+describe('issue #160', () => {
+  test('repro', async () => {
+    ;(globalThis as any).useReact19UseId()
+
+    const configAtom = atom('config', {
+      controls: [
+        {
+          counter: 0,
+          id: '1',
+        },
+        {
+          counter: 0,
+          id: '2',
+        },
+      ],
+    })
+
+    const useConfigAtomContext = () => useAtomContext(configAtom, true)
+
+    const useConfigAtomItems = (
+      instance: AtomInstanceType<typeof configAtom>
+    ) => instance.getState().controls ?? []
+
+    const selector = (
+      { get }: AtomGetters,
+      instance: AtomInstanceType<typeof configAtom>,
+      id: string
+    ) => get(instance).controls.find(t => t.id === id)
+
+    function Item({ id }: { id: string }) {
+      const instance = useConfigAtomContext()
+      const thisItem = useAtomSelector(selector, instance, id)
+
+      return <div data-testid={`item${id}`}>{thisItem?.counter}</div>
+    }
+
+    function Menu() {
+      const instance = useConfigAtomContext()
+      const items = useConfigAtomItems(instance)
+
+      return (
+        <div>
+          Menu: {instance.getState().controls[0].counter}
+          <ul>
+            {items?.map(item => (
+              <Item key={item.id} id={item.id} />
+            ))}
+          </ul>
+        </div>
+      )
+    }
+
+    function Content() {
+      const instance = useConfigAtomContext()
+      const items = useConfigAtomItems(instance)
+
+      return (
+        <div>
+          <span data-testid="content">
+            {instance.getState().controls[0].counter}
+          </span>
+          <ul>
+            {items?.map(item => (
+              <Item key={item.id} id={item.id} />
+            ))}
+          </ul>
+        </div>
+      )
+    }
+
+    function Outlet() {
+      const instance = useConfigAtomContext()
+
+      return (
+        <div>
+          <div>Outlet: {instance.getState().controls[0].counter}</div>
+          <Menu />
+          <Content />
+        </div>
+      )
+    }
+
+    function App() {
+      const instance = useAtomInstance(configAtom)
+
+      return (
+        <AtomProvider instance={instance}>
+          <Outlet />
+        </AtomProvider>
+      )
+    }
+
+    const { findByTestId, findAllByTestId } = renderInEcosystem(<App />, {
+      useStrictMode: true,
+    })
+
+    const content = await findByTestId('content')
+
+    expect(content.innerHTML).toBe('0')
+
+    const items1 = await findAllByTestId('item1')
+    const items2 = await findAllByTestId('item2')
+
+    expect(items1[0].innerHTML).toBe('0')
+    expect(items1[1].innerHTML).toBe('0')
+    expect(items2[0].innerHTML).toBe('0')
+    expect(items2[1].innerHTML).toBe('0')
+  })
+})
