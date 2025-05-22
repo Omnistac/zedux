e3bf0ba8654d61dbab67d5ece9c3ab15972c1d69
Joshua Claunch <bowheart31@gmail.com>
2025-05-22 11:18:59 -0400

    feat(atoms): dynamically update atom exports; improve export wrapping

--- packages/react/test/units/AtomApi.test.tsx
+++ packages/react/test/units/AtomApi.test.tsx
@@ -17,4 +17,51 @@ describe('AtomApi', () => {
     expect(node1.exports.fn).not.toBe(fn)
     expect(node1.exports.fn.name).toBe('fn')
   })
+
+  test('passing an api to api() clones it', () => {
+    const promise = Promise.resolve(1)
+    const api1 = api(1).setExports({ b: 2 }).setPromise(promise).setTtl(1000)
+    const api2 = api(api1)
+
+    expect(api1).not.toBe(api2)
+    expect(api1.exports).toEqual({ b: 2 })
+    expect(api1.promise).toBe(promise)
+    expect(api1.signal).toBe(undefined)
+    expect(api1.value).toBe(1)
+    expect(api1.ttl).toBe(1000)
+    expect(api2.exports).toEqual({ b: 2 })
+    expect(api2.promise).toBe(promise)
+    expect(api2.signal).toBe(undefined)
+    expect(api2.value).toBe(1)
+    expect(api2.ttl).toBe(1000)
+  })
+
+  test('functions with static properties are not wrapped', () => {
+    const atom1 = atom('1', () => {
+      const fn = () => {}
+      fn.staticProp = 1
+
+      return api().setExports({ fn })
+    })
+
+    const node1 = ecosystem.getNode(atom1)
+    const { fn } = node1.exports
+
+    expect(fn.staticProp).toBe(1)
+  })
+
+  test('classes themselves are not wrapped', () => {
+    const atom1 = atom('1', () => {
+      class Test {
+        prop = 1
+      }
+
+      return api().setExports({ Test })
+    })
+
+    const node1 = ecosystem.getNode(atom1)
+    const { Test } = node1.exports
+
+    expect(new Test().prop).toBe(1)
+  })
 })
